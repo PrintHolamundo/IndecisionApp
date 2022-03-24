cd81233590ae0149f84ce4a7f30f106575eadf3e
PrintHolamundo <jdohca@gmail.com>
2022-03-24 10:55:43 -0600

    Definir Pruebas a Realizar

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -10,4 +10,36 @@ describe("Indecision component",() => {
         test("debe de hacer match con el snapshot", () => {
             expect(wrapper.html()).toMatchSnapshot();
         })
+
+        test("Escribir en el input no debe de disparar nada (console.log)", () => {
+            // const input = wrapper.find("input");
+            // const spy = jest.spyOn(console, "log");
+            // input.setValue("Hola");
+            // expect(spy).not.toHaveBeenCalled();
+
+        })
+
+        test("Escribir el simbolo de '?' debe de disparar  el fetch", () => {
+            // const input = wrapper.find("input");
+            // const spy = jest.spyOn(wrapper.vm, "fetchData");
+            // input.setValue("?");
+            // expect(spy).toHaveBeenCalled();
+
+        })
+
+        test("pruebas en getAwswer ", () => {
+            // const spy = jest.spyOn(wrapper.vm, "getAnswer");
+            // wrapper.vm.getAnswer();
+            // expect(spy).toHaveBeenCalled();
+
+        })
+        
+
+        test("pruebas en getAwswer - Fallo en el API ", () => {
+            // const spy = jest.spyOn(wrapper.vm, "getAnswer");
+            // wrapper.vm.getAnswer();
+            // expect(spy).toHaveBeenCalled();
+
+        })
+
     })
