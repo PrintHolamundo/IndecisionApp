807340fa30de17672d4ede75512aad871a881c1a
PrintHolamundo <jdohca@gmail.com>
2022-03-25 10:08:50 -0600

    Spy con la instancia de Vue

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -1,46 +1,48 @@
 import { shallowMount, mount } from "@vue/test-utils";
 import Indecision from "@/components/Indecision.vue";
 
-describe("Indecision component",() => {
-        let wrapper;
-        let clgSpy;
-        beforeEach(() => {
-            wrapper = shallowMount(Indecision);
-            clgSpy = jest.spyOn(console, "log");
-        })
+describe("Indecision component", () => {
+    let wrapper;
+    let clgSpy;
+    beforeEach(() => {
+        wrapper = shallowMount(Indecision);
+        clgSpy = jest.spyOn(console, "log");
+    })
 
-        test("debe de hacer match con el snapshot", () => {
-            expect(wrapper.html()).toMatchSnapshot();
-        })
+    test("debe de hacer match con el snapshot", () => {
+        expect(wrapper.html()).toMatchSnapshot();
+    })
 
-        test("Escribir en el input no debe de disparar nada (console.log)", async() => {
-            const input = wrapper.find("input");
-            await input.setValue("Hola Mundo");
-            expect(clgSpy).toHaveBeenCalledTimes(1);
-           
-        })
-
-        test("Escribir el simbolo de '?' debe de disparar  el fetch", () => {
-            // const input = wrapper.find("input");
-            // const spy = jest.spyOn(wrapper.vm, "fetchData");
-            // input.setValue("?");
-            // expect(spy).toHaveBeenCalled();
-
-        })
-
-        test("pruebas en getAwswer ", () => {
-            // const spy = jest.spyOn(wrapper.vm, "getAnswer");
-            // wrapper.vm.getAnswer();
-            // expect(spy).toHaveBeenCalled();
-
-        })
-        
-
-        test("pruebas en getAwswer - Fallo en el API ", () => {
-            // const spy = jest.spyOn(wrapper.vm, "getAnswer");
-            // wrapper.vm.getAnswer();
-            // expect(spy).toHaveBeenCalled();
-
-        })
+    test("Escribir en el input no debe de disparar nada (console.log)", async () => {
+        const getAnswerSpy = jest.spyOn(wrapper.vm, "getAnswer");
+        const input = wrapper.find("input");
+        await input.setValue("Hola Mundo");
+        expect(clgSpy).toHaveBeenCalledTimes(1);
+        expect(getAnswerSpy).toHaveBeenCalledTimes(0);
 
     })
+
+    test("Escribir el simbolo de '?' debe de disparar  el fetch", () => {
+        // const input = wrapper.find("input");
+        // const spy = jest.spyOn(wrapper.vm, "fetchData");
+        // input.setValue("?");
+        // expect(spy).toHaveBeenCalled();
+
+    })
+
+    test("pruebas en getAwswer ", () => {
+        // const spy = jest.spyOn(wrapper.vm, "getAnswer");
+        // wrapper.vm.getAnswer();
+        // expect(spy).toHaveBeenCalled();
+
+    })
+
+
+    test("pruebas en getAwswer - Fallo en el API ", () => {
+        // const spy = jest.spyOn(wrapper.vm, "getAnswer");
+        // wrapper.vm.getAnswer();
+        // expect(spy).toHaveBeenCalled();
+
+    })
+
+})
